638dd9bc3b2f48b14c58dcdfb072c7f0dd724c5a
肖泽锋 <qq523107430@163.com>
2018-09-01 22:46:18 +0800

    自定义错误

--- handler/user/create.go
+++ handler/user/create.go
@@ -4,38 +4,44 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/lexkong/log"
+	"github.com/xiaozefeng/apiserver/handler"
 	"github.com/xiaozefeng/apiserver/pkg/errno"
-	"net/http"
 )
 
 func Create(c *gin.Context) {
-	var r struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
 
-	var err error
+	var r CreateRequest
 	if err := c.Bind(&r); err != nil {
-		c.JSON(http.StatusOK, gin.H{"error": errno.ErrBind})
+		handler.SendResponse(c, errno.ErrBind, nil)
 		return
 	}
 
-	log.Debugf("username is [%s], password is [%s]", r.Username, r.Password)
-	if r.Username == "" {
-		err = errno.New(errno.ErrUserNotFound,
-			fmt.Errorf("username can not found in db: xx.xx.xx.xx")).
-			Add("this is add message")
-		log.Errorf(err, "Get an error")
-	}
+	username := c.Param("username")
+	log.Infof("URL username :%s", username)
 
-	if errno.IsErrUserNotFound(err) {
-		log.Debug("err type is ErrUserNotFound")
+	desc := c.Query("desc")
+	log.Infof("URL key prams desc: %s", desc)
+
+	contentType := c.GetHeader("Content-Type")
+	log.Infof("Header Content-Type: %s ", contentType)
+
+	log.Debugf("username is: %s , password is %s ", r.Username, r.Password)
+
+	if r.Username == "" {
+		handler.SendResponse(c, errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")), nil)
+		return
 	}
 
 	if r.Password == "" {
-		err = fmt.Errorf("password is empty")
+		handler.SendResponse(c, fmt.Errorf("password is empty"), nil)
+		return
 	}
 
-	code, message := errno.DecodeErr(err)
-	c.JSON(http.StatusOK, gin.H{"code": code, "message": message})
+	rsp := CreateResponse{
+		Username: r.Username,
+	}
+
+	// Show the user information.
+	handler.SendResponse(c, nil, rsp)
+
 }
