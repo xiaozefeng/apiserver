3a93f160e80ba4ec3bc40f47c748f1ab56c4a404
肖泽锋 <qq523107430@163.com>
2018-09-01 14:26:26 +0800

    apisever demo

---
+++ router/router.go
@@ -0,0 +1,32 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/xiaozefeng/apiserver/handler/sd"
+	"github.com/xiaozefeng/apiserver/router/middleware"
+	"net/http"
+)
+
+func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
+	// Middleware
+	g.Use(gin.Recovery())
+	g.Use(middleware.NoCache)
+	g.Use(middleware.Options)
+	g.Use(middleware.Secure)
+	g.Use(mw...)
+	// 404 handler
+	g.NoRoute(func(context *gin.Context) {
+		context.String(http.StatusNotFound, "The incorrect API route")
+	})
+
+	// the health check handlers
+	group := g.Group("/sd")
+	{
+		group.GET("/health", sd.HealthCheck)
+		group.GET("/disk", sd.DiskCheck)
+		group.GET("/cpu", sd.CPUCheck)
+		group.GET("/ram", sd.RAMCheck)
+
+	}
+	return g
+}
